4d11d5318a66762498bb79dcb751da32da196e9c
software-experiment4 <se4@gmail.com>
2015-11-07 22:25:04 +0900

    Episode 4

--- startthedark/events/views.py
+++ startthedark/events/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from events.models import Event
 
-# Create your views here.
+def tonight(request):
+	events = Event.objects.filter(latest=True).today()
+	context = {
+		'events': events,
+	}
+	return render_to_response(
+		'tonight.html',
+		context,
+		context_instance = RequestContext(request)
+	)
