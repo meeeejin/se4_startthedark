edc90340dce77b05889f024208c49b10eaedded9
software-experiment4 <se4@gmail.com>
2015-11-13 13:48:16 +0900

    Episode 6

--- startthedark/events/views.py
+++ startthedark/events/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from events.models import Event
+from events.models import Event, Attendance
 from dateutil.parser import parse
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404
 from events.forms import EventForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def tonight(request):
 	events = Event.objects.filter(latest=True).today()
+	attending = []
+	for event in events:
+		try:
+			Attendance.objects.get(event=event, user=request.user)
+			attending.append(True)
+		except Attendance.DoesNotExist:
+			attending.append(False)
 	context = {
-		'events': events,
+		'events': zip(events, attending),
 	}
 	return render_to_response(
 		'tonight.html',
@@ -18,6 +27,7 @@ def tonight(request):
 		context_instance = RequestContext(request)
 	)
 
+@login_required
 def create(request):
 	form = EventForm(request.POST or None)
 	if form.is_valid():
@@ -43,3 +53,21 @@ def create(request):
 		{'form': form},
 		context_instance = RequestContext(request)
 	)
+
+@login_required
+def toggle_attendance(request):
+	try:
+		event_id = int(request.POST['event_id'])
+	except (KeyError, ValueError):
+		raise Http404
+	event = get_object_or_404(Event, id=event_id)
+	attendance, created = Attendance.objects.get_or_create(user=request.user, event=event)
+	if created:
+		messages.success(request, ('You are now attending "%s"' % event))
+	else:
+		attendance.delete()
+		messages.success(request, ('You are no longer attending "%s"' % event))
+	next = request.POST.get('next', '')
+	if not next:
+		next = request.META['HTTP_REFERER']
+	return HttpResponseRedirect(next)
